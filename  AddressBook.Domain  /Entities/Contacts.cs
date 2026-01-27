81b7e2b01deef0d623fe53f0d03a4a3f85eabb99
Bhanu Kiran <bhanukiran.g@keka.com>
2026-01-27 15:25:45 +0530

    Added git ignore file

---
+++  AddressBook.Domain  /Entities/Contacts.cs
@@ -0,0 +1,17 @@
+﻿namespace AddressBook.Domain.Entities
+{
+    public class Contacts
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
