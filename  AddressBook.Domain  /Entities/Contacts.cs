9137cba87eb94780899b40d6962915e297751311
Bhanu Kiran <bhanukiran.g@keka.com>
2026-01-28 16:31:30 +0530

    Refactor Contacts.cs

---  AddressBook.Domain  /Entities/Contacts.cs
+++  AddressBook.Domain  /Entities/Contacts.cs
@@ -3,15 +3,10 @@
     public class Contacts
     {
         public int Id { get; init; }
-
         public string Name { get; set; } = null!;
-
         public string? Email { get; set; }
-
         public string? Phone { get; set; }
-
         public DateTime CreatedAt { get; set; }
-
         public DateTime? UpdatedAt { get; set; }
     }
 }
